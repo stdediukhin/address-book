425eb29a21eb0c49b9dd26ccb6c30bba47ed7be2
Dmytro_Polianskyi <Dmytro_Polianskyi@epam.com>
2019-03-17 09:54:56 +0200

    [#10013] Spring Boot Actuator: - add HealthApiSpringBootTest

---
+++ src/test/java/com.epam.addressbook/HealthApiSpringBootTest.java
@@ -0,0 +1,35 @@
+package com.epam.addressbook;
+
+import com.jayway.jsonpath.DocumentContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static com.jayway.jsonpath.JsonPath.parse;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AddressBookApplication.class, webEnvironment = RANDOM_PORT)
+public class HealthApiSpringBootTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void healthTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/actuator/health", String.class);
+
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext healthJson = parse(response.getBody());
+
+        assertThat(healthJson.read("$.status", String.class)).isEqualTo("UP");
+    }
+}
